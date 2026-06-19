5319ea83211b46a73d31fa2290f0ce194f1c0ba5
thomas <th.fchs@gmail.com>
2026-06-19 11:34:48 +0200

    initial commit

---
+++ ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+  public:
+    ray() {}
+
+    ray(const point3& origin, const vec3& direction) : origin_(origin), direction_(direction) {}
+
+    const point3& origin() const  { return origin_; }
+    const vec3& direction() const { return direction_; }
+
+    point3 at(double t) const {
+        return origin_ + t*direction_;
+    }
+
+  private:
+    point3 origin_;
+    vec3 direction_;
+};
+
+#endif
