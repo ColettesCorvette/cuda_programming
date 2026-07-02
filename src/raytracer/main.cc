da8b10b8282584c8286c3cd5681150b000ce2996
thomas <th.fchs@gmail.com>
2026-07-02 17:58:02 +0200

    Rendering surface normals on a sphere + Ray-sphere intersection simplification

--- src/raytracer/main.cc
+++ src/raytracer/main.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include "color.h"
 #include "vec3.h"
@@ -15,20 +16,27 @@
 //   > 0 : le rayon entre et ressort (deux points, t1 < t2)
 // (on ne garde pour l'instant que l'existence d'une solution, pas la plus petite racine
 // positive qui donnerait le premier impact)
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+    
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std::sqrt(discriminant) ) / a;
+    }
 }
 
 color ray_color(const ray& r) {
-
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
-
+    auto t = hit_sphere((point3(0,0,-1)), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1,N.y()+1, N.z()+1);
+    }
+        
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
